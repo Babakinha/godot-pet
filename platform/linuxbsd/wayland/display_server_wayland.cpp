godotengine/godot

Testing out basic implementations
this is going fucking nowhere

platform/linuxbsd/wayland/display_server_wayland.cpp
@@ -213,6 +213,10 @@ bool DisplayServerWayland::has_feature(Feature p_feature) const {
 		} break;
 #endif
 
+		case FEATURE_WAYLAND_LAYER_SHELL: {
+			return wayland_thread.registry.wlr_layer_shell != nullptr;
+		} break;
+
 		default: {
 			return false;
 		}
@@ -1268,6 +1272,30 @@ bool DisplayServerWayland::window_get_flag(WindowFlags p_flag, DisplayServer::Wi
 
 	ERR_FAIL_COND_V(!windows.has(p_window_id), false);
 	return windows[p_window_id].flags & (1 << p_flag);
+}
+
+void DisplayServerWayland::window_set_wayland_layer(int p_layer, DisplayServer::WindowID p_window_id) {
+	MutexLock mutex_lock(wayland_thread.mutex);
+
+	ERR_FAIL_COND(!windows.has(p_window_id));
+	WindowData &wd = windows[p_window_id];
+
+	// Only allow setting layer on visible windows that support layer shell
+	if (wd.visible && wayland_thread.registry.wlr_layer_shell) {
+		wayland_thread.window_set_wayland_layer(p_window_id, p_layer);
+	}
+	
+	// Store the layer for when the window becomes visible
+	wd.wayland_layer = p_layer;
+}
+
+int DisplayServerWayland::window_get_wayland_layer(DisplayServer::WindowID p_window_id) const {
+	MutexLock mutex_lock(wayland_thread.mutex);
+
+	ERR_FAIL_COND_V(!windows.has(p_window_id), 0);
+	const WindowData &wd = windows[p_window_id];
+
+	return wd.wayland_layer;
 }
 
 void DisplayServerWayland::window_request_attention(DisplayServer::WindowID p_window_id) {

platform/linuxbsd/wayland/wayland_thread.cpp
@@ -4136,6 +4136,30 @@ bool WaylandThread::window_get_idle_inhibition(DisplayServer::WindowID p_window_
 	return ws.wp_idle_inhibitor != nullptr;
 }
 
+void WaylandThread::window_set_wayland_layer(DisplayServer::WindowID p_window_id, int p_layer) {
+	ERR_FAIL_COND(!windows.has(p_window_id));
+	WindowState &ws = windows[p_window_id];
+
+	// For now, we only store the layer. Layer shell protocol implementation 
+	// would require creating a different type of surface that's incompatible
+	// with regular XDG surfaces. This is a placeholder for future implementation.
+	ws.wayland_layer = p_layer;
+	
+	// TODO: Implement actual layer shell surface creation
+	// This would require:
+	// 1. Creating a zwlr_layer_surface_v1 instead of xdg_surface
+	// 2. Configuring the layer, anchor, size, etc.
+	// 3. Handling layer shell events
+	
+	print_line(vformat("WAYLAND LAYER: Set window %d layer to %d (placeholder implementation)", p_window_id, p_layer));
+}
+
+int WaylandThread::window_get_wayland_layer(DisplayServer::WindowID p_window_id) const {
+	ERR_FAIL_COND_V(!windows.has(p_window_id), 0);
+	const WindowState &ws = windows[p_window_id];
+	return ws.wayland_layer;
+}
+
 WaylandThread::ScreenData WaylandThread::screen_get_data(int p_screen) const {
 	ERR_FAIL_INDEX_V(p_screen, registry.wl_outputs.size(), ScreenData());
 

platform/linuxbsd/wayland/wayland_thread.h
@@ -70,6 +70,7 @@
 #include "wayland/protocol/xdg_foreign_v2.gen.h"
 #include "wayland/protocol/xdg_shell.gen.h"
 #include "wayland/protocol/xdg_system_bell.gen.h"
+#include "wayland/protocol/wlr_layer_shell.gen.h"
 
 // NOTE: Deprecated.
 #include "wayland/protocol/xdg_foreign_v1.gen.h"
@@ -221,6 +222,9 @@ public:
 		struct zwp_text_input_manager_v3 *wp_text_input_manager = nullptr;
 		uint32_t wp_text_input_manager_name = 0;
 
+		struct zwlr_layer_shell_v1 *wlr_layer_shell = nullptr;
+		uint32_t wlr_layer_shell_name = 0;
+
 		// We're really not meant to use this one directly but we still need to know
 		// whether it's available.
 		uint32_t wp_fifo_manager_name = 0;
@@ -236,6 +240,7 @@ public:
 		Rect2i rect;
 		DisplayServer::WindowMode mode = DisplayServer::WINDOW_MODE_WINDOWED;
 		bool suspended = false;
+		int wayland_layer = 0; // 0 = normal, 1 = background, 2 = bottom, 3 = top, 4 = overlay
 
 		// These are true by default as it isn't guaranteed that we'll find an
 		// xdg-shell implementation with wm_capabilities available. If and once we
@@ -258,6 +263,7 @@ public:
 		struct wl_surface *wl_surface = nullptr;
 		struct xdg_surface *xdg_surface = nullptr;
 		struct xdg_toplevel *xdg_toplevel = nullptr;
+		struct zwlr_layer_surface_v1 *wlr_layer_surface = nullptr;
 
 		struct wp_viewport *wp_viewport = nullptr;
 		struct wp_fractional_scale_v1 *wp_fractional_scale = nullptr;
@@ -592,8 +598,6 @@ private:
 	struct wl_seat *wl_seat_current = nullptr;
 
 	bool frame = true;
-
-	RegistryState registry;
 
 	bool initialized = false;
 
@@ -987,6 +991,7 @@ private:
 
 public:
 	Mutex &mutex = thread_data.mutex;
+	RegistryState registry;
 
 	struct wl_display *get_wl_display() const;
 
@@ -1044,6 +1049,8 @@ public:
 	void window_set_borderless(DisplayServer::WindowID p_window_id, bool p_borderless);
 	void window_set_title(DisplayServer::WindowID p_window_id, const String &p_title);
 	void window_set_app_id(DisplayServer::WindowID p_window_id, const String &p_app_id);
+	void window_set_wayland_layer(DisplayServer::WindowID p_window_id, int p_layer);
+	int window_get_wayland_layer(DisplayServer::WindowID p_window_id) const;
 
 	bool window_is_focused(DisplayServer::WindowID p_window_id);
 

scene/main/window.cpp
@@ -570,6 +570,22 @@ bool Window::get_flag(Flags p_flag) const {
 	return flags[p_flag];
 }
 
+void Window::set_wayland_layer(WaylandLayer p_layer) {
+	ERR_MAIN_THREAD_GUARD;
+	wayland_layer = p_layer;
+	
+	if (window_id != DisplayServer::INVALID_WINDOW_ID && DisplayServer::get_singleton()->has_feature(DisplayServer::FEATURE_WAYLAND_LAYER_SHELL)) {
+		if (!is_in_edited_scene_root()) {
+			DisplayServer::get_singleton()->window_set_wayland_layer(p_layer, window_id);
+		}
+	}
+}
+
+Window::WaylandLayer Window::get_wayland_layer() const {
+	ERR_READ_THREAD_GUARD_V(WAYLAND_LAYER_NORMAL);
+	return wayland_layer;
+}
+
 bool Window::is_maximize_allowed() const {
 	ERR_READ_THREAD_GUARD_V(false);
 	if (window_id != DisplayServer::INVALID_WINDOW_ID) {
@@ -3129,6 +3145,9 @@ void Window::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_flag", "flag", "enabled"), &Window::set_flag);
 	ClassDB::bind_method(D_METHOD("get_flag", "flag"), &Window::get_flag);
 
+	ClassDB::bind_method(D_METHOD("set_wayland_layer", "layer"), &Window::set_wayland_layer);
+	ClassDB::bind_method(D_METHOD("get_wayland_layer"), &Window::get_wayland_layer);
+
 	ClassDB::bind_method(D_METHOD("is_maximize_allowed"), &Window::is_maximize_allowed);
 
 	ClassDB::bind_method(D_METHOD("request_attention"), &Window::request_attention);
@@ -3310,6 +3329,7 @@ void Window::_bind_methods() {
 	ADD_PROPERTYI(PropertyInfo(Variant::BOOL, "minimize_disabled"), "set_flag", "get_flag", FLAG_MINIMIZE_DISABLED);
 	ADD_PROPERTYI(PropertyInfo(Variant::BOOL, "maximize_disabled"), "set_flag", "get_flag", FLAG_MAXIMIZE_DISABLED);
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "force_native"), "set_force_native", "get_force_native");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "wayland_layer", PROPERTY_HINT_ENUM, "Normal,Background,Bottom,Top,Overlay"), "set_wayland_layer", "get_wayland_layer");
 
 	ADD_GROUP("Limits", "");
 	ADD_PROPERTY(PropertyInfo(Variant::VECTOR2I, "min_size", PROPERTY_HINT_NONE, "suffix:px"), "set_min_size", "get_min_size");
@@ -3403,6 +3423,12 @@ void Window::_bind_methods() {
 	BIND_ENUM_CONSTANT(WINDOW_INITIAL_POSITION_CENTER_OTHER_SCREEN);
 	BIND_ENUM_CONSTANT(WINDOW_INITIAL_POSITION_CENTER_SCREEN_WITH_MOUSE_FOCUS);
 	BIND_ENUM_CONSTANT(WINDOW_INITIAL_POSITION_CENTER_SCREEN_WITH_KEYBOARD_FOCUS);
+
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_NORMAL);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_BACKGROUND);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_BOTTOM);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_TOP);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_OVERLAY);
 
 	GDVIRTUAL_BIND(_get_contents_minimum_size);
 

Babakinha/godot-pet

platform/linuxbsd/wayland/display_server_wayland.cpp
@@ -331,6 +331,10 @@ bool DisplayServerWayland::has_feature(DisplayServerEnums::Feature p_feature) co
 			return AccessibilityServer::get_singleton()->is_supported();
 		} break;
 
+		case DisplayServerEnums::FEATURE_WAYLAND_LAYER_SHELL: {
+			return wayland_thread.registry.wlr_layer_shell != nullptr;
+		} break;
+
 		default: {
 			return false;
 		}
@@ -1377,6 +1381,30 @@ bool DisplayServerWayland::window_get_flag(DisplayServerEnums::WindowFlags p_fla
 
 	ERR_FAIL_COND_V(!windows.has(p_window_id), false);
 	return windows[p_window_id].flags & (1 << p_flag);
+}
+
+void DisplayServerWayland::window_set_wayland_layer(int p_layer, DisplayServerEnums::WindowID p_window_id) {
+	MutexLock mutex_lock(wayland_thread.mutex);
+
+	ERR_FAIL_COND(!windows.has(p_window_id));
+	WindowData &wd = windows[p_window_id];
+
+	// Only allow setting layer on visible windows that support layer shell
+	if (wd.visible && wayland_thread.registry.wlr_layer_shell) {
+		wayland_thread.window_set_wayland_layer(p_window_id, p_layer);
+	}
+
+	// Store the layer for when the window becomes visible
+	wd.wayland_layer = p_layer;
+}
+
+int DisplayServerWayland::window_get_wayland_layer(DisplayServerEnums::WindowID p_window_id) const {
+	MutexLock mutex_lock(wayland_thread.mutex);
+
+	ERR_FAIL_COND_V(!windows.has(p_window_id), 0);
+	const WindowData &wd = windows[p_window_id];
+
+	return wd.wayland_layer;
 }
 
 void DisplayServerWayland::window_request_attention(DisplayServerEnums::WindowID p_window_id) {

platform/linuxbsd/wayland/wayland_thread.cpp
@@ -5294,6 +5294,30 @@ void WaylandThread::window_set_color_profile(DisplayServerEnums::WindowID p_wind
 	wp_image_description_v1_destroy(image_desc);
 }
 
+void WaylandThread::window_set_wayland_layer(DisplayServerEnums::WindowID p_window_id, int p_layer) {
+	ERR_FAIL_COND(!windows.has(p_window_id));
+	WindowState &ws = windows[p_window_id];
+
+	// For now, we only store the layer. Layer shell protocol implementation
+	// would require creating a different type of surface that's incompatible
+	// with regular XDG surfaces. This is a placeholder for future implementation.
+	ws.wayland_layer = p_layer;
+
+	// TODO: Implement actual layer shell surface creation
+	// This would require:
+	// 1. Creating a zwlr_layer_surface_v1 instead of xdg_surface
+	// 2. Configuring the layer, anchor, size, etc.
+	// 3. Handling layer shell events
+
+	print_verbose(vformat("WAYLAND LAYER: Set window %d layer to %d (placeholder implementation)", p_window_id, p_layer));
+}
+
+int WaylandThread::window_get_wayland_layer(DisplayServerEnums::WindowID p_window_id) const {
+	ERR_FAIL_COND_V(!windows.has(p_window_id), 0);
+	const WindowState &ws = windows[p_window_id];
+	return ws.wayland_layer;
+}
+
 WaylandThread::ScreenData WaylandThread::screen_get_data(int p_screen) const {
 	ERR_FAIL_INDEX_V(p_screen, registry.wl_outputs.size(), ScreenData());
 

platform/linuxbsd/wayland/wayland_thread.h
@@ -76,6 +76,7 @@
 #include "wayland/protocol/xdg_shell.gen.h"
 #include "wayland/protocol/xdg_system_bell.gen.h"
 #include "wayland/protocol/xdg_toplevel_icon.gen.h"
+#include "wayland/protocol/wlr_layer_shell.gen.h"
 
 // NOTE: Deprecated.
 #include "wayland/protocol/xdg_foreign_v1.gen.h"
@@ -277,6 +278,9 @@ public:
 		struct wp_pointer_warp_v1 *wp_pointer_warp = nullptr;
 		uint32_t wp_pointer_warp_name = 0;
 
+		struct zwlr_layer_shell_v1 *wlr_layer_shell = nullptr;
+		uint32_t wlr_layer_shell_name = 0;
+
 		// We're really not meant to use this one directly but we still need to know
 		// whether it's available.
 		uint32_t wp_fifo_manager_name = 0;
@@ -307,6 +311,7 @@ public:
 		bool tiled_top = false;
 		bool tiled_bottom = false;
 		bool suspended = false; // We can stop drawing.
+		int wayland_layer = 0; // 0 = normal, 1 = background, 2 = bottom, 3 = top, 4 = overlay
 
 		// These are true by default as it isn't guaranteed that we'll find an
 		// xdg-shell implementation with wm_capabilities available. If and once we
@@ -333,6 +338,7 @@ public:
 		struct wl_surface *wl_surface = nullptr;
 		struct xdg_surface *xdg_surface = nullptr;
 		struct xdg_toplevel *xdg_toplevel = nullptr;
+		struct zwlr_layer_surface_v1 *wlr_layer_surface = nullptr;
 
 		struct wp_viewport *wp_viewport = nullptr;
 		struct wp_fractional_scale_v1 *wp_fractional_scale = nullptr;
@@ -728,8 +734,6 @@ private:
 	DisplayServerEnums::WindowID hovered_window_id = DisplayServerEnums::INVALID_WINDOW_ID;
 
 	bool frame = true;
-
-	RegistryState registry;
 
 	bool initialized = false;
 
@@ -1240,6 +1244,7 @@ private:
 
 public:
 	Mutex &mutex = thread_data.mutex;
+	RegistryState registry;
 
 	struct wl_display *get_wl_display() const;
 
@@ -1303,6 +1308,9 @@ public:
 
 	void window_set_max_size(DisplayServerEnums::WindowID p_window_id, const Size2i &p_size);
 	void window_set_min_size(DisplayServerEnums::WindowID p_window_id, const Size2i &p_size);
+
+	void window_set_wayland_layer(DisplayServerEnums::WindowID p_window_id, int p_layer);
+	int window_get_wayland_layer(DisplayServerEnums::WindowID p_window_id) const;
 
 	bool window_can_set_mode(DisplayServerEnums::WindowID p_window_id, DisplayServerEnums::WindowMode p_window_mode) const;
 	void window_try_set_mode(DisplayServerEnums::WindowID p_window_id, DisplayServerEnums::WindowMode p_window_mode);

scene/main/window.cpp
@@ -625,6 +625,22 @@ float Window::get_output_max_linear_value() const {
 	return 1.0f;
 }
 
+void Window::set_wayland_layer(WaylandLayer p_layer) {
+	ERR_MAIN_THREAD_GUARD;
+	wayland_layer = p_layer;
+
+	if (window_id != DisplayServerEnums::INVALID_WINDOW_ID && DisplayServer::get_singleton()->has_feature(DisplayServerEnums::FEATURE_WAYLAND_LAYER_SHELL)) {
+		if (!is_in_edited_scene_root()) {
+			DisplayServer::get_singleton()->window_set_wayland_layer(p_layer, window_id);
+		}
+	}
+}
+
+Window::WaylandLayer Window::get_wayland_layer() const {
+	ERR_READ_THREAD_GUARD_V(WAYLAND_LAYER_NORMAL);
+	return wayland_layer;
+}
+
 bool Window::is_maximize_allowed() const {
 	ERR_READ_THREAD_GUARD_V(false);
 	if (window_id != DisplayServerEnums::INVALID_WINDOW_ID) {
@@ -3418,6 +3434,9 @@ void Window::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_fullscreen_shortcut_enabled", "enabled"), &Window::set_fullscreen_shortcut_enabled);
 	ClassDB::bind_method(D_METHOD("is_fullscreen_shortcut_enabled"), &Window::is_fullscreen_shortcut_enabled);
 
+	ClassDB::bind_method(D_METHOD("set_wayland_layer", "layer"), &Window::set_wayland_layer);
+	ClassDB::bind_method(D_METHOD("get_wayland_layer"), &Window::get_wayland_layer);
+
 	ClassDB::bind_method(D_METHOD("is_maximize_allowed"), &Window::is_maximize_allowed);
 
 	ClassDB::bind_method(D_METHOD("request_attention"), &Window::request_attention);
@@ -3606,6 +3625,7 @@ void Window::_bind_methods() {
 	ADD_PROPERTYI(PropertyInfo(Variant::BOOL, "maximize_disabled"), "set_flag", "get_flag", FLAG_MAXIMIZE_DISABLED);
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "fullscreen_shortcut_enabled"), "set_fullscreen_shortcut_enabled", "is_fullscreen_shortcut_enabled");
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "force_native"), "set_force_native", "get_force_native");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "wayland_layer", PROPERTY_HINT_ENUM, "Normal,Background,Bottom,Top,Overlay"), "set_wayland_layer", "get_wayland_layer");
 
 	ADD_GROUP("Limits", "");
 	ADD_PROPERTY(PropertyInfo(Variant::VECTOR2I, "min_size", PROPERTY_HINT_NONE, "suffix:px"), "set_min_size", "get_min_size");
@@ -3704,6 +3724,12 @@ void Window::_bind_methods() {
 	BIND_ENUM_CONSTANT(WINDOW_INITIAL_POSITION_CENTER_OTHER_SCREEN);
 	BIND_ENUM_CONSTANT(WINDOW_INITIAL_POSITION_CENTER_SCREEN_WITH_MOUSE_FOCUS);
 	BIND_ENUM_CONSTANT(WINDOW_INITIAL_POSITION_CENTER_SCREEN_WITH_KEYBOARD_FOCUS);
+
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_NORMAL);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_BACKGROUND);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_BOTTOM);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_TOP);
+	BIND_ENUM_CONSTANT(WAYLAND_LAYER_OVERLAY);
 
 	GDVIRTUAL_BIND(_get_contents_minimum_size);
 